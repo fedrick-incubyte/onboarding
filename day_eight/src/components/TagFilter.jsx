136d260f5016eaf615050c18e9a2debcccd6e6b1
Fedrick Nishant <fedrick@incubyte.co>
2026-05-07 19:58:59 +0530

    feat: TagFilter calls onTagSelect when a tag badge is clicked

--- day_eight/src/components/TagFilter.jsx
+++ day_eight/src/components/TagFilter.jsx
@@ -1,12 +1,12 @@
 import { Badge } from './ui/Badge.jsx'
 import { getAllTags } from '../domain/tag.js'
 
-export function TagFilter({ entries }) {
+export function TagFilter({ entries, onTagSelect }) {
   const tags = getAllTags(entries)
   return (
     <div>
-      <Badge label="All" />
-      {tags.map(tag => <Badge key={tag} label={tag} />)}
+      <Badge label="All" clickable onClick={() => onTagSelect(null)} />
+      {tags.map(tag => <Badge key={tag} label={tag} clickable onClick={() => onTagSelect(tag)} />)}
     </div>
   )
 }
