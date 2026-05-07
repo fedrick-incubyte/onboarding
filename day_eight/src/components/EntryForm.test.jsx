adf2d1cfc3da25a11af498a68daad8d74b02edb1
Fedrick Nishant <fedrick@incubyte.co>
2026-05-07 19:47:14 +0530

    test: should render a title input

---
+++ day_eight/src/components/EntryForm.test.jsx
@@ -0,0 +1,11 @@
+import { describe, it, expect, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { EntryForm } from './EntryForm.jsx'
+
+describe('EntryForm', () => {
+  it('should render a title input', () => {
+    render(<EntryForm onSubmit={() => {}} />)
+    expect(screen.getByPlaceholderText('Title')).toBeInTheDocument()
+  })
+})
